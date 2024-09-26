098f5c170b318b3ca9c7a590faf5beb3527a6351
dyu <david.yu.ftw@gmail.com>
2024-09-26 15:09:39 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+CURRENT_DIR=$PWD
+# locate
+if [ -z "$BASH_SOURCE" ]; then
+    SCRIPT_DIR=`dirname "$(readlink -f $0)"`
+elif [ -e '/bin/zsh' ]; then
+    F=`/bin/zsh -c "print -lr -- $BASH_SOURCE(:A)"`
+    SCRIPT_DIR=`dirname $F`
+elif [ -e '/usr/bin/realpath' ]; then
+    F=`/usr/bin/realpath $BASH_SOURCE`
+    SCRIPT_DIR=`dirname $F`
+else
+    F=$BASH_SOURCE
+    while [ -h "$F" ]; do F="$(readlink $F)"; done
+    SCRIPT_DIR=`dirname $F`
+fi
+# change pwd
+cd $SCRIPT_DIR
+
+cleancss -o dist/bundle.min.css -O2 removeDuplicateRules:on dist/bundle.css && \
+uglifyjs dist/bundle.js -o dist/bundle.min.js && \
+sed -E 's/dist\/bundle/bundle\.min/g' < index.html > dist/index.html
